e2b5e659b78890caf8954e9a8daf9e0fffa4be95
AdminAkai <akatoirotech@gmail.com>
2026-08-20 03:06:22 -0400

    Adjusted spacing for form

--- src/features/Transmission/Transmission.tsx
+++ src/features/Transmission/Transmission.tsx
@@ -1,4 +1,6 @@
-import { FC, useEffect, useState } from 'react'
+import { FC } from 'react'
+
+import { Link } from 'react-router-dom'
 
 import PageMark from 'src/shared/components/PageMark/PageMark'
 import Section from 'src/shared/components/Section/Section'
@@ -7,42 +9,62 @@ import { Language } from 'src/shared/redux/settingsSlice/settingsInitial'
 import { useAppSelector } from 'src/shared/redux/store'
 import { selectLanguage } from 'src/shared/redux/settingsSlice/settingsSelectors'
 
-import { taglineSubjects } from './lib'
-
 import styles from './Transmission.module.css'
 
 const Transmission: FC = () => {
-  const [currentIndex, setCurrentIndex] = useState<number>(0)
+  // const [currentIndex, setCurrentIndex] = useState<number>(0)
 
   const lang: Language = useAppSelector(selectLanguage)
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prev) => (prev + 1) % taglineSubjects[lang].length)
-    }, 2000)
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrentIndex((prev) => (prev + 1) % taglineSubjects[lang].length)
+  //   }, 2000)
 
-    return () => clearInterval(interval)
-  }, [])
+  //   return () => clearInterval(interval)
+  // }, [])
 
   return (
     <Section>
-      <div className={styles.contact}>
-        <div className={styles['contact-sector']}>
-          <h1 className={styles['contact-title']}>
+      <div className={styles.transmission}>
+        <div className={styles['transmission-sector']}>
+          <h1 className={styles['transmission-title']}>
             [TRANSMISSION / OPEN CHANNEL]
           </h1>
-          <p>BROADCAST YOUR SIGNAL.</p>
-        </div>
-        <div className={styles['contact-sector']}>
-          <div className={styles['contact-form-header']}>
-            <p>MESSAGE / 3 FIELDS</p>
-            <p>CHANNEL OPEN</p>
+          <p className={styles['transmission-subtitle']}>
+            BROADCAST YOUR SIGNAL.
+          </p>
+          <div className={styles['transmission-links']}>
+            LINKEDIN
+            <Link
+              to='https://www.linkedin.com/in/josh-adrian-trinidad/'
+              className={styles['transmission-link']}
+            >
+              /in/josh-adrian-trinidad/
+            </Link>
           </div>
-          <form className={styles['contact-form']}>
-            <label htmlFor='test'>test</label>
-            <input id='test' className={styles['contact-form-input']}></input>
-            <label htmlFor='test'>test</label>
-            <input></input>
+        </div>
+        <div className={styles['transmission-sector']}>
+          <div className={styles['transmission-form-header']}>
+            <p className={styles['transmission-form-header-title']}>
+              MESSAGE / 3 FIELDS
+            </p>
+            <p className={styles['transmission-form-header-title']}>
+              [CHANNEL OPEN]
+            </p>
+          </div>
+          <form className={styles['transmission-form']}>
+            <label
+              htmlFor='name'
+              className={styles['transmission-form-input-label']}
+            >
+              [01] / NAME
+            </label>
+            <input
+              id='name'
+              className={styles['transmission-form-input']}
+              placeholder='YOUR NAME'
+            ></input>
           </form>
         </div>
       </div>
